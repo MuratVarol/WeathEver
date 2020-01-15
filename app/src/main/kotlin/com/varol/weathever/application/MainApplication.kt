9610d5fa3813fc2339b72f12b791008f035ddfbf
murat.varol <murat.varol@adesso.com.tr>
2020-01-15 15:34:06 +0300

    stetho imp and room version inc.

--- app/src/main/kotlin/com/varol/weathever/application/MainApplication.kt
+++ app/src/main/kotlin/com/varol/weathever/application/MainApplication.kt
@@ -1,9 +1,12 @@
 package com.varol.weathever.application
 
+import com.facebook.stetho.Stetho
 import com.varol.weathever.internal.di.DaggerApplication
 
 class MainApplication : DaggerApplication() {
     override fun onCreate() {
         super.onCreate()
+
+        Stetho.initializeWithDefaults(this)
     }
 }
