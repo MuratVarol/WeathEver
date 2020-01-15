ee602267604456bf628a68f45a49e78bde2af56a
murat.varol <murat.varol@adesso.com.tr>
2020-01-15 15:31:38 +0300

    parsing handler

--- app/src/main/kotlin/com/varol/weathever/data/remote/ServiceRequestHandler.kt
+++ app/src/main/kotlin/com/varol/weathever/data/remote/ServiceRequestHandler.kt
@@ -5,11 +5,7 @@ import com.squareup.moshi.JsonDataException
 import com.varol.weathever.internal.util.Failure
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.plugins.RxJavaPlugins
 import io.reactivex.schedulers.Schedulers
-import java.io.IOException
-import java.net.SocketException
-import java.util.concurrent.TimeoutException
 
 /**
  * Singleton class for handling API requests and responses
@@ -24,23 +20,22 @@ object ServiceRequestHandler {
     fun <T> sendRequest(call: Single<T>): Single<Either<Failure, T>> {
         return call
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
             .map<Either<Failure, T>> {
                 Either.Right(it)
             }
             .onErrorResumeNext { throwable: Throwable ->
                 Single.just(
-                    Either.Left(handleParsingErrors(throwable))
+                    Either.Left(toFailure(throwable))
                 )
             }
-            .doOnError { throwable: Throwable -> Either.Left(handleParsingErrors(throwable)) }
+            .doOnError { throwable: Throwable -> Either.Left(toFailure(throwable)) }
     }
 }
 
-private fun handleParsingErrors(throwable: Throwable): Failure {
+private fun toFailure(throwable: Throwable): Failure {
     return when (throwable) {
         is JsonParseException, is JsonDataException ->
             Failure.ParsingDataError
-        else -> Failure.UnknownError()
+        else -> Failure.UnknownError(throwable.localizedMessage)
     }
 }
