c459999a7bd8f522d54e17d2b78ae84b769976ef
murat.varol <murat.varol@adesso.com.tr>
2020-01-18 15:55:28 +0300

    data null check control added to onActivityCreated, periodic download logic changed

--- app/src/main/kotlin/com/varol/weathever/screen/weather/WeatherViewModel.kt
+++ app/src/main/kotlin/com/varol/weathever/screen/weather/WeatherViewModel.kt
@@ -3,42 +3,39 @@ package com.varol.weathever.screen.weather
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import com.varol.weathever.base.BaseAndroidViewModel
+import com.varol.weathever.data.entity.WeatherListItemViewEntity
 import com.varol.weathever.data.entity.WeatherViewEntity
-import com.varol.weathever.domain.GetLocationWeatherUseCase
-import com.varol.weathever.domain.SveCurrentWeatherToDbUseCase
+import com.varol.weathever.domain.*
+import com.varol.weathever.internal.popup.PopupCallback
+import com.varol.weathever.internal.popup.PopupUiModel
 import com.varol.weathever.internal.util.SingleLiveData
+import com.varol.weathever.screen.weather.weather_home.WeatherDirections
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-open class WeatherViewModel @Inject constructor(
+private const val CONST_HUNDRED = 100
+
+const val PERIODIC_FETCH_INTERVAL = 15L // a minute
+
+class WeatherViewModel @Inject constructor(
     context: Context,
     private val weatherByLoc: GetLocationWeatherUseCase,
-    private val saveWeather: SveCurrentWeatherToDbUseCase
+    private val saveWeather: SaveCurrentWeatherToDbUseCase,
+    private val savedWeathers: GetSavedWeatherListUseCase,
+    private val weatherByCityId: GetWeatherByCityIdUseCase,
+    private val updateWeather: UpdateWeatherOnDbUseCase
 ) : BaseAndroidViewModel(context) {
 
     val currentLocWeather = MutableLiveData<WeatherViewEntity>()
-    val isSaveButtonEnabled = SingleLiveData<Boolean>().apply { value = false }
+    val isSaveButtonEnabled = SingleLiveData<Boolean>()
+        .apply { value = false }
     val progress = MutableLiveData<Int>()
-    private val timerDisposableComposite = CompositeDisposable()
+    val savedWeatherList = MutableLiveData<MutableList<WeatherListItemViewEntity>>()
+    val remainingSeconds = MutableLiveData<String>()
+    val timerDisposableComposite = CompositeDisposable()
 
-    fun getWeatherByLocationPeriodically(
-        lat: Double,
-        lon: Double,
-        interval: Long,
-        timeUnit: TimeUnit = TimeUnit.SECONDS
-    ) {
-        timerToLoadIdleFragment(interval, timeUnit)
-        getWeatherByLocation(lat, lon)
-        disposables.add(
-            startCountdown(interval, timeUnit)
-                .subscribe {
-                    getWeatherByLocation(lat, lon)
-                })
-    }
-
-    private fun getWeatherByLocation(lat: Double, lon: Double) {
+    fun getWeatherByLocation(lat: Double, lon: Double) {
         showProgress()
         disposables.add(
             weatherByLoc.getWeatherByLocation(lat, lon)
@@ -48,28 +45,71 @@ open class WeatherViewModel @Inject constructor(
 
     private fun handleSuccessGetLocWeather(weatherViewEntity: WeatherViewEntity) {
         hideProgress()
-        reFireTimerForProgress()
+        setNextRefreshTimer(
+            weatherViewEntity.coordinates.lat,
+            weatherViewEntity.coordinates.lon,
+            PERIODIC_FETCH_INTERVAL
+        )
         currentLocWeather.postValue(weatherViewEntity)
         isSaveButtonEnabled.postValue(true)
     }
 
-    private fun reFireTimerForProgress() {
-        timerDisposableComposite.clear()
-        timerToLoadIdleFragment(PERIODIC_FETCH_INTERVAL)
-    }
-
-    private fun timerToLoadIdleFragment(
+    private fun setNextRefreshTimer(
+        lat: Double,
+        lon: Double,
         countDownTime: Long,
         timeUnit: TimeUnit = TimeUnit.SECONDS
     ) {
-        progress.postValue(100)
-        val multiplier = 100.div(countDownTime.toDouble())
+        val multiplier = CONST_HUNDRED.div(countDownTime.toDouble())
         var tickCount: Int = countDownTime.toInt()
         val disposable = startTimer(1, timeUnit, countDownTime).subscribe {
-            progress.postValue((--tickCount * multiplier).toInt())
+            remainingSeconds.postValue((--tickCount).toString())
+            progress.postValue((tickCount * multiplier).toInt())
+            if (tickCount <= 0) {
+                getWeatherByLocation(lat, lon)
+                resetProgressStatus()
+            }
         }
+        timerDisposableComposite.clear()
         timerDisposableComposite.add(disposable)
     }
+
+    fun resetProgressStatus() {
+        progress.postValue(CONST_HUNDRED)
+        remainingSeconds.postValue(PERIODIC_FETCH_INTERVAL.toString())
+    }
+
+    fun deleteWeatherItemFromDb(id: Long) {
+        navigate(
+            PopupUiModel(
+                message = "$id'li kayıt silinecektir!\nEmin misiniz?",
+                addCancelButton = true
+            ),
+            object : PopupCallback {
+                override fun onConfirmClick() {
+                    showInformBar("onConfirmClick: $id")
+                }
+
+                override fun onCancelClick() {
+                    showInformBar("onCancelClick: $id")
+                }
+            }
+        )
+    }
+
+    fun refreshWeatherItem(id: Long) {
+        showProgress()
+        disposables.add(
+            weatherByCityId.getWeatherByCityId(id)
+                .subscribe { value -> value.either(::handleFailure, ::handleUpdatedWeather) }
+        )
+    }
+
+    private fun handleUpdatedWeather(weatherViewEntity: WeatherViewEntity) {
+        hideProgress()
+        updateWeather.updateWeatherOnDb(weatherViewEntity)
+    }
+
 
     fun saveCurrentWeatherToDb() {
         currentLocWeather.value?.let { weather ->
@@ -79,14 +119,37 @@ open class WeatherViewModel @Inject constructor(
                     .observeOn(getMainThreadScheduler())
                     .subscribeOn(getBackgroundScheduler())
                     .subscribe({ rowIndex ->
-                        hideProgress()
                         showInformBar("Saved")
                     }, { ex ->
                         showErrorBar(ex.localizedMessage)
                     })
             )
+            hideProgress()
         } ?: run {
             showErrorBar("Hmm, Current weather seems null")
         }
+    }
+
+    fun getWeathersFromDb() {
+        showProgress()
+        disposables.add(
+            savedWeathers.getSavedWeathers()
+                .observeOn(getMainThreadScheduler())
+                .subscribeOn(getBackgroundScheduler())
+                .subscribe({ savedWeathers ->
+                    savedWeatherList.postValue(savedWeathers.toMutableList())
+                }, { ex ->
+                    showErrorBar(ex.localizedMessage)
+                })
+        )
+        hideProgress()
+    }
+
+    fun toSavedWeatherList() {
+        navigate(WeatherDirections.toWeatherList)
+    }
+
+    fun onWeatherSelect(cityId: Long) {
+
     }
 }
--- app/src/main/kotlin/com/varol/weathever/screen/weather/weather_home/WeatherFragment.kt
+++ app/src/main/kotlin/com/varol/weathever/screen/weather/weather_home/WeatherFragment.kt
@@ -1,57 +1,74 @@
-package com.varol.weathever.screen.weather
+package com.varol.weathever.screen.weather.weather_home
 
 import android.Manifest
-import android.app.Activity
 import android.content.Context.LOCATION_SERVICE
 import android.content.Intent
-import android.content.IntentSender
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
-import androidx.fragment.app.FragmentActivity
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.varol.weathever.R
 import com.varol.weathever.base.BaseFragment
 import com.varol.weathever.databinding.FragmentWeatherBinding
 import com.varol.weathever.internal.extension.showPopup
 import com.varol.weathever.internal.extension.showToast
+import com.varol.weathever.internal.listeners.ToolbarListener
 import com.varol.weathever.internal.popup.PopupCallback
 import com.varol.weathever.internal.popup.PopupUiModel
+import com.varol.weathever.screen.weather.WeatherViewModel
 import com.yanzhenjie.permission.AndPermission
 import com.yanzhenjie.permission.RequestExecutor
 
-
-const val PERIODIC_FETCH_INTERVAL = 60L
 const val LOCATION_REQUEST_CODE = 1001
+
+object WeatherDirections {
+    val toWeatherList = WeatherFragmentDirections.actionWeatherFragmentToWeatherListFragment()
+}
 
 class WeatherFragment : BaseFragment<WeatherViewModel, FragmentWeatherBinding>() {
 
     override val layoutId: Int
         get() = R.layout.fragment_weather
 
+    private val toolbarCallback = object : ToolbarListener {
+        override fun onRightIconClicked() {
+            viewModel.toSavedWeatherList()
+        }
+    }
+
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val locationManager: LocationManager? by lazy {
         context?.getSystemService(LOCATION_SERVICE) as LocationManager
     }
 
+    override fun initialize() {
+        binder.apply {
+            callback = toolbarCallback
+        }
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-            AndPermission.with(this)
-                .runtime()
-                .permission(Manifest.permission.ACCESS_FINE_LOCATION)
-                .rationale { _, _, executor ->
-                    showNeedLocationPermissionDialog(executor)
-                }
-                .onGranted {
-                    setLocationListenerIfGpsEnabled()
-                }
-                .onDenied {
-                    showEnablePermissionInSettingsDialog()
-                }.start()
+
+        // we have the data, so we have loc.
+        // no need to set loc listener and fetch data on return of list fragment
+        if (viewModel.currentLocWeather.value != null)
+            return
+
+        AndPermission.with(this)
+            .runtime()
+            .permission(Manifest.permission.ACCESS_FINE_LOCATION)
+            .rationale { _, _, executor ->
+                showNeedLocationPermissionDialog(executor)
+            }
+            .onGranted {
+                setLocationListenerIfGpsEnabled()
+            }
+            .onDenied {
+                showEnablePermissionInSettingsDialog()
+            }.start()
     }
 
     private fun setLocationListenerIfGpsEnabled() {
@@ -64,19 +81,18 @@ class WeatherFragment : BaseFragment<WeatherViewModel, FragmentWeatherBinding>()
 
     private fun setLocationListener() {
         activity?.let { activity ->
-        fusedLocationClient = LocationServices
-            .getFusedLocationProviderClient(activity).apply {
-                lastLocation.addOnSuccessListener { location: Location? ->
-                    location?.let {
-                        context.showToast(it.latitude.toString() + " " + location.longitude.toString())
-                        viewModel.getWeatherByLocationPeriodically(
-                            it.latitude,
-                            it.longitude,
-                            PERIODIC_FETCH_INTERVAL
-                        )
+            fusedLocationClient = LocationServices
+                .getFusedLocationProviderClient(activity).apply {
+                    lastLocation.addOnSuccessListener { location: Location? ->
+                        location?.let {
+                            context.showToast(it.latitude.toString() + " " + location.longitude.toString())
+                            viewModel.getWeatherByLocation(
+                                it.latitude,
+                                it.longitude
+                            )
+                        }
                     }
                 }
-            }
         } ?: kotlin.run {
             viewModel.showErrorBar("Oops so strange!, Maybe restart app?")
         }
