893d5ad994f3992b1ffec4a3a7223ff4f447f793
murat.varol <murat.varol@adesso.com.tr>
2020-01-18 15:53:07 +0300

    navigation for popup dialog

--- app/src/main/kotlin/com/varol/weathever/internal/navigation/NavigationCommand.kt
+++ app/src/main/kotlin/com/varol/weathever/internal/navigation/NavigationCommand.kt
@@ -1,6 +1,8 @@
 package com.varol.weathever.internal.navigation
 
 import androidx.navigation.NavDirections
+import com.varol.weathever.internal.popup.PopupCallback
+import com.varol.weathever.internal.popup.PopupUiModel
 
 /**
  * A simple sealed class to handle more properly
@@ -9,4 +11,6 @@ import androidx.navigation.NavDirections
 sealed class NavigationCommand {
     data class To(val directions: NavDirections) : NavigationCommand()
     object Back : NavigationCommand()
+    data class Popup(val model: PopupUiModel, val callback: PopupCallback? = null) :
+        NavigationCommand()
 }
