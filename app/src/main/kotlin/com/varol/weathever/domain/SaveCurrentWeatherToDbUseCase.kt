c535d89a3081a012ca5db7a6dfd0ee765fda2c80
murat.varol <murat.varol@adesso.com.tr>
2020-01-18 15:51:24 +0300

    rename typo

--- app/src/main/kotlin/com/varol/weathever/domain/SaveCurrentWeatherToDbUseCase.kt
+++ app/src/main/kotlin/com/varol/weathever/domain/SaveCurrentWeatherToDbUseCase.kt
@@ -5,7 +5,7 @@ import com.varol.weathever.data.repository.WeatherRepository
 import io.reactivex.Single
 import javax.inject.Inject
 
-class SveCurrentWeatherToDbUseCase @Inject constructor(
+class SaveCurrentWeatherToDbUseCase @Inject constructor(
     private val weatherRepository: WeatherRepository
 ) {
     fun saveWeatherToDb(
